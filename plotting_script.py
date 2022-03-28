a3d158340b2bb1c5f95bda54cd1a109e72c57e60
abatten <adamjbatten@gmail.com>
2022-03-28 16:22:33 +1100

    Inital commit of plotting script

---
+++ plotting_script.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x_values = np.linspace(-1, 1, 100)
+
+x_squr = x_values**2
+x_cube = x_values**3
+
+fig, ax = plt.subplots(nrows=1, ncols=1)
+ax.plot(x_values, x_values, label="Linear")
+ax.plot(x_values, x_squr, label="Quadratic")
+ax.plot(x_values, x_cube, label="Cubic")
+ax.legend(frameon=False)
+plt.savefig("functions.png")
+
+
