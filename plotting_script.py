d7acc5f15d26460b3bd1058a7c77834de3f3dcfb
abatten <adamjbatten@gmail.com>
2022-03-28 16:25:05 +1100

    Changed color of linear line

--- plotting_script.py
+++ plotting_script.py
@@ -8,7 +8,7 @@ x_squr = x_values**2
 x_cube = x_values**3
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.plot(x_values, x_values, label="Linear")
+ax.plot(x_values, x_values, color="black", label="Linear")
 ax.plot(x_values, x_squr, label="Quadratic")
 ax.plot(x_values, x_cube, label="Cubic")
 ax.legend(frameon=False)
